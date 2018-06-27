378c551c65e4e511c9c7f2d8b812642dbf6ec8dc
Arpit <rpittrer@gmail.com>
2018-06-27 17:36:42 +0530

    plot

--- more.py
+++ more.py
@@ -3,12 +3,17 @@ import numpy as np
 
 img = np.zeros((512, 512, 3), np.uint8)
 
-cv.imshow("original image", img)
+# cv.imshow("original image", img)
 # cv.rectangle(img, (100, 100), (200, 200), (0, 255, 0), -1)
 # cv.circle(img, (300, 300), (100), (127, 50, 127), -1)
 
-pts = np.array([[100, 100], [300, 90], [400, 500]], )
-cv.polylines(img, [pts], True, (0, 255, 0), 1)
+pts = np.array([[100, 100], [200, 100], [150, 190]])
+ptr = np.array([[150, 70], [100, 160], [200, 160]])
+cv.polylines(img, [pts], True, (0, 255, 0), -3)
+cv.polylines(img, [ptr], True, (0, 255, 0), 3)
+
+font = cv.FONT_HERSHEY_SIMPLEX
+cv.putText(img, "Arpit", (333, 333), font, 1, (250, 0, 0), 1, 1, 1)
 
 pts = pts.reshape((-1, 1, 2))
 
